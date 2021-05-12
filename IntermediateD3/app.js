8a1e525612692c4143a06cde6497edc4e7df3670
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-11 20:14:06 -0500

    Axes and Gridlines

--- IntermediateD3/app.js
+++ IntermediateD3/app.js
@@ -1,7 +1,7 @@
 const
     width = 500,
     height = 500,
-    padding = 20;
+    padding = 30;
 
 const yScale = d3.scaleLinear()
     .domain(d3.extent(birthData2011, d => d.lifeExpectancy))
@@ -11,6 +11,14 @@ const xScale = d3.scaleLinear()
     .domain(d3.extent(birthData2011, d => d.births / d.population))
     .range([padding, width - padding]);
 
+const xAxis = d3.axisBottom(xScale)
+    .tickSize(-height + 2 * padding)
+    .tickSizeOuter(0);
+
+const yAxis = d3.axisLeft(yScale)
+    .tickSize(-width + 2 * padding)
+    .tickSizeOuter(0);
+
 const colorScale = d3.scaleLinear()
     .domain(d3.extent(birthData2011, d => d.population / d.area))
     .range(['lightgreen', 'black']);
@@ -18,6 +26,18 @@ const colorScale = d3.scaleLinear()
 const radiusScale = d3.scaleLinear()
     .domain(d3.extent(birthData2011, d => d.births))
     .range([2, 40]);
+
+d3.select('svg')
+    .append('g')
+    .attr('transform', 'translate(0,' + (
+        height - padding) + ')')
+    .call(xAxis);
+
+d3.select('svg')
+    .append('g')
+    .attr('transform', 'translate(' + padding
+        + ', 0)')
+    .call(yAxis);
 
 d3.select('svg')
     .attr('width', width)
@@ -30,3 +50,28 @@ d3.select('svg')
     .attr('cy', d => yScale(d.lifeExpectancy))
     .attr('fill', d => colorScale(d.population / d.area))
     .attr('r', d => radiusScale(d.births));
+
+d3.select('svg')
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', height - padding)
+    .attr('dy', '1.5em')
+    .style('text-anchor', 'middle')
+    .text('Births per Capita');
+
+d3.select('svg')
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', padding)
+    .style('text-anchor', 'middle')
+    .style('font-size', '1.5em')
+    .text('Data on Births by Country in 2011');
+
+d3.select('svg')
+    .append('text')
+    .attr('transform', 'rotate(-90')
+    .attr('x', - height / 2)
+    .attr('y', padding)
+    .attr('dy', '-1.1em')
+    .style('text-anchor', 'middle')
+    .text('Life Expectancy');
