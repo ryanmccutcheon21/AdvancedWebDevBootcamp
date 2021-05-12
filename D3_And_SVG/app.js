67ed470ddb4bfbbb592971ca6b37e17a80adcf37
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-05-11 19:25:58 -0500

    Extrema and Scales

--- D3_And_SVG/app.js
+++ D3_And_SVG/app.js
@@ -1,11 +1,22 @@
 const
-    minYear = birthData[0].year,
-    maxYear = birthData[birthData.length - 1].year,
+    minYear = d3.min(birthData, d => {
+        return d.year;
+    }),
+    maxYear = d3.max(birthData, d => {
+        return d.year;
+    }),
     width = 600,
     height = 600,
     barPadding = 10,
     numBars = 12,
-    barWidth = width / numBars - barPadding;
+    barWidth = width / numBars - barPadding,
+    maxBirths = d3.max(birthData, d => {
+        return d.births;
+    });
+
+const yScale = d3.scaleLinear()
+    .domain([0, maxBirths])
+    .range([height, 0])
 
 d3.select('input')
     .property('min', minYear)
@@ -23,12 +34,28 @@ d3.select('svg')
     .append('rect')
     .attr('width', barWidth)
     .attr('height', d => {
-        return d.births / 2.5e6 * height;
+        return height - yScale(d.births);
     })
     .attr('y', d => {
-        return height - d.births / 2.5e6 * height;
+        return yScale(d.births);
     })
     .attr('x', (d, i) => {
         return barWidth + barPadding * i;
     })
     .attr('fill', 'purple')
+
+
+d3.select('input')
+    .on('input', () => {
+        let year = +d3.event.target.value;
+        d3.selectAll('rect')
+            .data(birthData.filter(d => {
+                return d.year === year;
+            }))
+            .attr('height', d => {
+                return height - yScale(d.births);
+            })
+            .attr('y', d => {
+                return yScale(d.births);
+            })
+    })
